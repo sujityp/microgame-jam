f796b3826985780103476dfa6d6e1ed10eba1dca
wer8888 <91093536+plagakit@users.noreply.github.com>
2024-01-05 15:57:42 -0500

    Main functionality done

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -6,12 +6,16 @@ class_name Main
 var current_mg : Microgame
 var mg_won : bool = false
 var score : int = 0
+var time_scale : float = 1
 
 @onready var room_anim = $Room/AnimationPlayer
-@onready var timer : Timer = $Timer
+@onready var win_sound = $Win
+@onready var lose_sound = $Loss
+@onready var next_sound = $Next
 
 func _ready():
-	timer.start()
+	load_random_microgame()
+	Engine.time_scale = time_scale
 
 
 func load_random_microgame():
@@ -26,6 +30,7 @@ func load_microgame(microgame : PackedScene):
 	inst.finish_game.connect(on_microgame_done)
 	inst.win_game.connect(func(): mg_won = true)
 	inst.lose_game.connect(func(): pass) # losing doesnt change a win
+	mg_won = false
 	
 	room_anim.play("zoom_in")
 	add_child(inst)
@@ -35,21 +40,26 @@ func on_microgame_done():
 	# if not already won, look at the timeout setting
 	var won = mg_won or (not current_mg.lose_on_timeout)
 	
+	room_anim.play("zoom_out")
+	await room_anim.animation_finished
+	unload_current_microgame()
+	
 	if won:
 		score += 1
 	else:
 		pass
 	
-	room_anim.play("zoom_out")
-	await room_anim.animation_finished
-	unload_current_microgame()
-	timer.start()
+	if won:
+		win_sound.play()
+	else:
+		lose_sound.play()
+		
+	await get_tree().create_timer(1.75).timeout
+	next_sound.play()
+	await get_tree().create_timer(1).timeout
+	load_random_microgame()
 
 
 func unload_current_microgame():
 	current_mg.queue_free()
 	current_mg = null
-
-
-func _on_timer_timeout():
-	load_random_microgame()
